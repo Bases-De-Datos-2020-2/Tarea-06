3872546a637fd90c4527a8fde2f089b2c5103c95
David Morales <david_ivan2402@ciencias.unam.mx>
2020-05-26 02:29:26 -0500

    Se agrego la creacion de tablas y relaciones

--- SQL/DML.sql
+++ SQL/DML.sql
@@ -4,8 +4,8 @@ CREATE DATABASE hospital
     ENCODING = 'UTF8'
     CONNECTION LIMIT = -1;
 
- --
-CREATE TABLE public."Persona"
+ -- Tablas de la base de datos
+CREATE TABLE public."persona"
 (
     id serial,
     nombre character varying(50),
@@ -21,12 +21,12 @@ CREATE TABLE public."Persona"
     CONSTRAINT person_pkey PRIMARY KEY (id)
 );
 
-ALTER TABLE public."Persona"
+ALTER TABLE public."persona"
     OWNER to postgres;
 
 --
 
-CREATE TABLE public."Ingreso"
+CREATE TABLE public."ingreso"
 (
     num_ingreso serial,
     motivo character varying,
@@ -38,41 +38,133 @@ CREATE TABLE public."Ingreso"
     PRIMARY KEY (num_ingreso)
 );
 
-ALTER TABLE public."Ingreso"
+ALTER TABLE public."ingreso"
     OWNER to postgres;
 
 -- 
-CREATE TABLE public."medico"
-(
-    id_medico bigint
-)
-
-
-ALTER TABLE public."medico"
-    OWNER to postgres;
+CREATE TABLE medico() INHERITS (persona);
+ALTER TABLE public.medico
+    ADD CONSTRAINT medico_pkey PRIMARY KEY (id);
 
 --
-CREATE TABLE public."Consulta"
+CREATE TABLE public."consulta"
 (
     num_consulta serial,
     consultorio bigint,
     fecha date,
     precio integer,
     id_persona bigint,
-    id_medivo bigint,
+    id_medico bigint,
     CONSTRAINT consulta_pkey PRIMARY KEY (num_consulta)
 );
 
-ALTER TABLE public."Consulta"
+ALTER TABLE public."consulta"
     OWNER to postgres;
 
 -- 
-CREATE TABLE public."Especialidad"
+CREATE TABLE public."especialidad"
 (
     id serial,
     nombre_especialidad character varying,
     CONSTRAINT especialidad_pkey PRIMARY KEY (id)
 );
 
-ALTER TABLE public."Especialidad"
+ALTER TABLE public."especialidad"
     OWNER to postgres;
+
+-- 
+CREATE TABLE public.supervisar
+(
+    id_medico_supervisor integer,
+    id_medico_supervisado integer
+);
+
+ALTER TABLE public.supervisar
+    OWNER to postgres;
+
+
+--
+CREATE TABLE public.tener
+(
+    id_medico integer,
+    id_especialidad integer
+);
+
+ALTER TABLE public.tener
+    OWNER to postgres;
+
+
+
+
+-- Relaciones
+
+-- Relacion persona -> ingreso 
+ALTER TABLE public.ingreso
+    ADD CONSTRAINT persona_ingreso_fk FOREIGN KEY (id_persona)
+    REFERENCES public.persona (id) MATCH SIMPLE
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+    NOT VALID;
+
+
+
+-- Relacion medico -> ingreso
+ALTER TABLE public.ingreso
+    ADD CONSTRAINT medico_ingreso_fk FOREIGN KEY (id_medico)
+    REFERENCES public.medico (id) MATCH SIMPLE
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+    NOT VALID;
+
+-- Relacion medico -> consulta 
+ALTER TABLE public.consulta
+    ADD CONSTRAINT medico_consulta FOREIGN KEY (id_medico)
+    REFERENCES public.medico (id) MATCH SIMPLE
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+    NOT VALID;
+
+
+-- Relacion persona -> consulta 
+ALTER TABLE public.consulta
+    ADD CONSTRAINT "person_consulta " FOREIGN KEY (id_persona)
+    REFERENCES public.persona (id) MATCH SIMPLE
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+    NOT VALID;
+
+
+-- Relacion  medico -> medico recursiva
+ALTER TABLE public.supervisar
+    ADD CONSTRAINT medico_supervisor_medico_supervisado_fk FOREIGN KEY (id_medico_supervisor)
+    REFERENCES public.medico (id) MATCH SIMPLE
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+    NOT VALID;
+
+ALTER TABLE public.supervisar
+    ADD CONSTRAINT medico_supervisado_medico_supervisor_fk FOREIGN KEY (id_medico_supervisado)
+    REFERENCES public.medico (id) MATCH SIMPLE
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+    NOT VALID;
+
+
+-- Relacion especialidad -> tener
+ALTER TABLE public.tener
+    ADD CONSTRAINT especialidad_tener_fkey FOREIGN KEY (id_especialidad)
+    REFERENCES public.especialidad (id) MATCH SIMPLE
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+    NOT VALID;
+
+-- Relacion medico -> tener 
+
+ALTER TABLE public.tener
+    ADD CONSTRAINT medico_tener_fkey FOREIGN KEY (id_medico)
+    REFERENCES public.medico (id) MATCH SIMPLE
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+    NOT VALID;
+
+
